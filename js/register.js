d892c91dc69e35521fa5bff2ec49ac1da9017b0b
Administrator <317395616@qq.com>
2018-01-16 10:29:01 +0800

    注册页面，表单验证register.js已完成，（对用户名、密码、及用户电话进行了自定义验证格式）   ----刘康

--- js/register.js
+++ js/register.js
@@ -9,9 +9,10 @@ $.validator.setDefaults({
 
 
 // 就绪函数
-$().ready(function(){
-    //让当前表单调用validate方法，实现表单验证功能
+    $().ready(function(){
+        //让当前表单调用validate方法，实现表单验证功能
     $("#commentForm").validate({
+        focusCleanup:true,
         onfocusout: function(element) { $(element).valid(); },
         rules: {     //配置验证规则，key就是被验证的dom对象，value就是调用验证的方法(也是json格式)
             username: {
@@ -25,25 +26,28 @@ $().ready(function(){
                 rangelength: [6, 12]
             },
             phone: {
-                // required: true,
-                // minlength: 11,
-                // maxlength:11,
+                required: true,
+                minlength: 11,
+                maxlength:11,
                 isphoneNum:true
             }
         },
         messages: {
             username: {
                 required: "*请输入用户名（必填）",
-                rangelength: $.validator.format("*用户名长度在必须为：{0}-{1}之间")
+                rangelength: $.validator.format("*用户名长度在必须为：{0}-{1}之间"),
+                checkName:"*只允许6-16位英文字母、数字或者下画线！"
             },
             password: {
                 required: "*请输入密码（必填）",
-                rangelength: $.validator.format("*字段长度必须为：{0}-{1}之间")
+                rangelength: $.validator.format("*字段长度必须为：{0}-{1}之间"),
+                checkPwd:"*只允许2-10位英文字母、数字或者下画线！"
             },
             phone: {
-                // required: "*请输入用户电话号码（必填）"
-                // minlength:"请您输入11位手机号码",
-                // maxlength:"请您输入11位手机号码"
+                required: "*请输入用户电话号码（必填）",
+                minlength:"请您输入11位手机号码",
+                maxlength:"请您输入11位手机号码",
+                isphoneNum:"*您输入的手机号码格式不正确"
             }
         }
     });
@@ -54,18 +58,17 @@ $().ready(function(){
     $.validator.addMethod("checkName",function(value,element,params){
         var checkName = /^\w{2,10}$/g;
         return this.optional(element)||(checkName.test(value));
-    },"*只允许2-10位英文字母、数字或者下画线！");
+    });
     //《------------------自定义密码验证-------------------》
     $.validator.addMethod("checkPwd",function(value,element,params){
         var checkPwd = /^\w{6,16}$/g;
         return this.optional(element)||(checkPwd.test(value));
-    },"*只允许6-16位英文字母、数字或者下画线！");
+    });
     //《-----------------自定义电话验证-------------------》
     $.validator.addMethod("isphoneNum", function(value, element,params) {
         // debugger;
         // var length = value.length;
         var mobile = /^1(3|4|5|7|8)\d{9}$/;
-
         return this.optional(element) || (mobile.test(value));
     });
 });//ready
