a39dbfb800c354ad42b240fbe1fd6e945321933c
Administrator <317395616@qq.com>
2018-01-16 11:56:51 +0800

    统一cart_addItems.js   ----刘康

--- js/cart_addItems.js
+++ js/cart_addItems.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
     //《-----------------购物车-----------------》
-var count = 0;
-$("#detail-cart-btn").click(function () {
-    // alert("加入购物车");
-    count++;
+var count = 1;
+$(".item_add").click(function () {
+
+    alert("加入购物车成功");
+
     //将此值放入session作用域中
-    $("#detail-cart-num").text(count);
+    $(".card_num").text(count++);
 });
 
 });//ready
