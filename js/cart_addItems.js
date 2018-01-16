a2dbb3567e484cac03fa6ec4e746eb07732850d2
677jiayou <444718843@qq.com>
2018-01-16 15:55:16 +0800

    修改cart.html页面，为购物车页面cart.html的商品的增加，减少，删除，商品数量，订单总价标签添加id， 为包含商品信息的div标签增加id。 修改cart_additems.js,实现商品数量的增加，减少和商品的删除，并动态改变商品数量和订单总价。

--- js/cart_addItems.js
+++ js/cart_addItems.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
     //《-----------------购物车-----------------》
+    //count为购物车总数量
 var count = 1;
-var item_count=1;
+//item_count6为购物车内商品夜礼服的数量
+var item_count6=1;
+//item_count5为购物车内商品芒果列车的数量
+var item_count5=1;
+//item_count4为购物车内商品玫瑰物语的数量
+var item_count4=1;
+//item_count3为购物车内商品留恋之恋的数量
+var item_count3=1;
+//totalPrice为购物车内商品总价
+var totalPrice;
 $(".item_add").click(function () {
 
     alert("加入购物车成功");
@@ -9,11 +19,128 @@ $(".item_add").click(function () {
     //将此值放入session作用域中
     $(".card_num").text(count++);
 });
-    // 购物车点击增加，减少，商品数量变动
-$(".btn-info").click(function () {
-    item_count++,
-    $(".cart_item_num").text("数量："+item_count);
-});
+
+
+    /*购物车内每次点击某个商品的增加按钮，商品数量数量增加1个,并且重新计算总价*/
+    //商品夜礼服的增加按钮功能
+    $("#btn-info6").click(function () {
+        item_count6++;
+        $(".cart_item_num6").text("数量："+item_count6);
+        //重新计算订单总金额totalPrice
+        totalPrice=item_count6*299+item_count5*269+item_count4*299+item_count3*229;
+        $("#cart_items_totalPrice").text("订单总金额: ¥"+totalPrice);
+    });
+    //商品芒果列车的增加按钮功能
+    $("#btn-info5").click(function () {
+        item_count5++;
+            $(".cart_item_num5").text("数量："+item_count5);
+        //重新计算订单总金额totalPrice
+        totalPrice=item_count6*299+item_count5*269+item_count4*299+item_count3*229;
+        $("#cart_items_totalPrice").text("订单总金额: ¥"+totalPrice);
+    });
+    //商品玫瑰物语的增加按钮功能
+    $("#btn-info4").click(function () {
+        item_count4++;
+            $(".cart_item_num4").text("数量："+item_count4);
+        //重新计算订单总金额totalPrice
+        totalPrice=item_count6*299+item_count5*269+item_count4*299+item_count3*229;
+        $("#cart_items_totalPrice").text("订单总金额: ¥"+totalPrice);
+    });
+    //商品留恋之恋的增加按钮功能
+    $("#btn-info3").click(function () {
+        item_count3++;
+            $(".cart_item_num3").text("数量："+item_count3);
+        //重新计算订单总金额totalPrice
+        totalPrice=item_count6*299+item_count5*269+item_count4*299+item_count3*229;
+        $("#cart_items_totalPrice").text("订单总金额: ¥"+totalPrice);
+    });
+
+
+    /*购物车内每次点击某个商品的减少按钮，商品数量数量减少1个,并且重新计算总价*/
+    //商品夜礼服的减少按钮功能
+    $("#btn-warning6").click(function () {
+        item_count6--;
+        //当商品数量减少到0或者小于0的时候，我们设置其为0
+        if(parseInt(eval(item_count6))<=0){
+            item_count6=0;
+        }
+            $(".cart_item_num6").text("数量："+item_count6);
+        //重新计算订单总金额totalPrice
+        totalPrice=item_count6*299+item_count5*269+item_count4*299+item_count3*229;
+        $("#cart_items_totalPrice").text("订单总金额: ¥"+totalPrice);
+    });
+    //商品芒果列车的减少按钮功能
+    $("#btn-warning5").click(function () {
+        item_count5--;
+        //当商品数量减少到0或者小于0的时候，我们设置其为0
+        if(parseInt(eval(item_count5))<=0){
+            item_count5=0;
+        }
+            $(".cart_item_num5").text("数量："+item_count5);
+        //重新计算订单总金额totalPrice
+        totalPrice=item_count6*299+item_count5*269+item_count4*299+item_count3*229;
+        $("#cart_items_totalPrice").text("订单总金额: ¥"+totalPrice);
+    });
+    //商品玫瑰物语的减少按钮功能
+    $("#btn-warning4").click(function () {
+        item_count4--;
+        //当商品数量减少到0或者小于0的时候，我们设置其为0
+        if(parseInt(eval(item_count4))<=0){
+            item_count4=0;
+        }
+            $(".cart_item_num4").text("数量："+item_count4);
+        //重新计算订单总金额totalPrice
+        totalPrice=item_count6*299+item_count5*269+item_count4*299+item_count3*229;
+        $("#cart_items_totalPrice").text("订单总金额: ¥"+totalPrice);
+    });
+    //商品留恋之恋的减少按钮功能
+    $("#btn-warning3").click(function () {
+        item_count3--;
+        //当商品数量减少到0或者小于0的时候，我们设置其为0
+        if(parseInt(eval(item_count3))<=0){
+            item_count3=0;
+        }
+            $(".cart_item_num3").text("数量："+item_count3);
+        //重新计算订单总金额totalPrice
+        totalPrice=item_count6*299+item_count5*269+item_count4*299+item_count3*229;
+        $("#cart_items_totalPrice").text("订单总金额: ¥"+totalPrice);
+    });
+
+
+    /*购物车内每次点击某个商品的删除按钮，则将此商品移除,并且重新计算总价*/
+    //商品夜礼服的删除按钮功能
+    $("#btn-danger6").click(function () {
+        $("#item_6").remove();
+        //重新计算订单总金额totalPrice
+        item_count6=0;
+        totalPrice=item_count6*299+item_count5*269+item_count4*299+item_count3*229;
+        $("#cart_items_totalPrice").text("订单总金额: ¥"+totalPrice);
+    });
+    //商品芒果列车的删除按钮功能
+    $("#btn-danger5").click(function () {
+        $("#item_5").remove();
+        //重新计算订单总金额totalPrice
+        item_count5=0;
+        totalPrice=item_count6*299+item_count5*269+item_count4*299+item_count3*229;
+        $("#cart_items_totalPrice").text("订单总金额: ¥"+totalPrice);
+    });
+    //商品玫瑰物语的删除按钮功能
+    $("#btn-danger4").click(function () {
+        $("#item_4").remove();
+        //重新计算订单总金额totalPrice
+        item_count4=0;
+        totalPrice=item_count6*299+item_count5*269+item_count4*299+item_count3*229;
+        $("#cart_items_totalPrice").text("订单总金额: ¥"+totalPrice);
+    });
+    //商品留恋之恋的删除按钮功能
+    $("#btn-danger3").click(function () {
+        $("#item_3").remove();
+        //重新计算订单总金额totalPrice
+        item_count3=0;
+        totalPrice=item_count6*299+item_count5*269+item_count4*299+item_count3*229;
+        $("#cart_items_totalPrice").text("订单总金额: ¥"+totalPrice);
+    });
+
 });//ready
 
 
